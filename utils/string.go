d079aacf89e8faa98f9226f984486a78774f0293
zhuangyuetao <zhuhai90tao@163.com>
2018-11-24 02:00:45 +0800

    add SplitMap by string

--- utils/string.go
+++ utils/string.go
@@ -3,6 +3,9 @@ package utils
 import (
 	"bytes"
 	"math/rand"
+	"strings"
+	// "unicode"
+	// "fmt"
 )
 
 var BaseChars []string = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
@@ -17,4 +20,28 @@ func RandString(length int, chars []string) string {
 		buf.WriteString(chars[t])
 	}
 	return buf.String()
+}
+
+// 裁剪成map
+func SplitMap(str string, sepA string, sepB string) *map[string]string {
+	mmap := make(map[string]string)
+
+	// 遍历第一层解析
+	sstrs := strings.Split(str, sepA)
+	slen := len(sstrs)
+	for i := 0; i < slen; i++ {
+		// fmt.Println(i, sstrs[i], strings.FieldsFunc(sstrs[i], unicode.IsSpace))
+		// 解析第二层
+		sargs := strings.Split(sstrs[i], sepB)
+		alen := len(sargs)
+		if alen <= 0 {
+			mmap[sstrs[i]] = "" // 空值
+			continue
+		} else if alen == 1 {
+			mmap[sargs[0]] = "" // 空值
+			continue
+		}
+		mmap[sargs[0]] = sargs[1]
+	}
+	return &mmap
 }
