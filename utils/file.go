1df7c925eedee3ac8de50c19ec0c4a537bed4118
zhuangyuetao <zhuhai90tao@163.com>
2018-11-27 01:14:24 +0800

    upload file

--- utils/file.go
+++ utils/file.go
@@ -5,8 +5,8 @@ import (
 )
 
 // 文件夹是否存在
-func FolderExist(path string) bool {
-	_, err := os.Stat(path)
+func FolderExist(filedir string) bool {
+	_, err := os.Stat(filedir)
 	if err == nil {
 		return true
 	}
@@ -15,3 +15,18 @@ func FolderExist(path string) bool {
 	}
 	return false
 }
+
+// 文件夹不存在则创建
+func FolderNoCreate(filedir string) {
+	if FolderExist(filedir) {
+		return // 已经存在
+	}
+	os.Mkdir(filedir, os.ModePerm)
+}
+
+// 文件夹创建例子
+// "path/filepath"
+// dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+// if err != nil {
+// 	log.Fatal(err)
+// }
